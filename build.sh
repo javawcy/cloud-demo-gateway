e7157be545100311eb52825b5bee885b3c9bdd84
Chongyu <javawcy@outlook.com>
2020-09-22 16:34:00 +0800

    fix: dynamic routes

--- build.sh
+++ build.sh
@@ -1,9 +1,11 @@
+version=$1
+
 echo "=======gateway======="
 
 mvn clean install -Dmaven.test.skip=true
 
-docker build -f docker/dev.Dockerfile -t 192.168.10.124:8889/public/gateway:latest .
-docker push 192.168.10.124:8889/public/gateway:latest
-docker rmi -f 192.168.10.124:8889/public/gateway:latest
+docker build -f docker/dev.Dockerfile -t 192.168.10.124:8889/public/gateway:"${version}" .
+docker push 192.168.10.124:8889/public/gateway:"${version}"
+docker rmi -f 192.168.10.124:8889/public/gateway:"${version}"
 
 #docker run -d -p 9771:8080 --name gateway1 --hostname gateway1 --network cloud-demo 192.168.10.124:8889/public/gateway:latest
